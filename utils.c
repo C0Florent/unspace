d228285685bfbe8225b8968c746aba5911da0f76
Florent Charpentier <florent.charpentier@epitech.eu>
2025-09-04 15:50:22 +1000

    feat: add dry_run in show_options

--- utils.c
+++ utils.c
@@ -9,6 +9,7 @@ void show_inputs(struct unspace_input const *input)
     printf("verbose: %i\n", input->o.verbose);
     printf("recursive: %i\n", input->o.recursive);
     printf("dump_input: %i\n", input->dump_input);
+    printf("dry_run: %i\n", input->o.dry_run);
     printf("replace: %c\n", input->o.replace);
     printf("files:\n");
     for (size_t i = 0; i < input->filec; i++) {
